3e550a3f22b7c80f68e8c73b43ca8dd863536cc6
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-05-18 21:42:19 -0300

    interação cor script

--- script1.js/script.js
+++ script1.js/script.js
@@ -1,4 +1,13 @@
-function logar() {
-    location.href = "inde2.html"
+var a = window.document.getElementById('area');
+
+a.addEventListener('mouseenter', entrar);
+a.addEventListener('mouseout', sair);
+
+function entrar() {
+    a.style.backgroundColor = 'green';
+}
+
+function sair() {
+    a.style.backgroundColor = 'yellow';
 }
 
