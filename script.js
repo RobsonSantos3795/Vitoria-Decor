f5ef5b2f18bff89882cf9904026a355981568a8d
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-05-20 01:24:23 -0300

    arquivos adicionados

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ menuButton.addEventListener("click", function() {
     menu.style.display = "block";
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function() {
+  var container = document.getElementById('container');
+  var scrollAmount = 1; // Quantidade de pixels para rolar
+  var delay = 50; // Delay em milissegundos entre cada movimento
+  //Define o intervalo de tempo em milissegundos entre cada movimento de rolagem. Neste caso, é 50 milissegundos (0,05 segundos).
+
+  function scrollHorizontally() {  //será responsável por mover a rolagem horizontalmente.
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+          container.scrollLeft = 0;  //Se o contêiner estiver no final do conteúdo, a rolagem é reiniciada para o início (posição 0)
+      } else {
+          container.scrollLeft += scrollAmount;
+      }
+  }
+
+  var scrollInterval = setInterval(scrollHorizontally, delay);
+}
