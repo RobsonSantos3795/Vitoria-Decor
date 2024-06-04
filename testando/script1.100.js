5982a342b445e69e196c6e19ab28d094aa218c5a
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-06-04 01:09:12 -0300

    resconstrução1.1

--- testando/script1.100.js
+++ testando/script1.100.js
@@ -22,3 +22,21 @@ menuButton.addEventListener("click", function() {
     menu.style.display = "block";
   }
 });
+
+//Sessão abaixo contem a construção do rolamento de imagem
+window.onload = function() {
+  var container = document.getElementById('container');
+  var scrollAmount = 1; // Quantidade de pixels para rolar
+  var delay = 30; // Delay em milissegundos entre cada movimento
+  //Define o intervalo de tempo em milissegundos entre cada movimento de rolagem. Neste caso, é 50 milissegundos (0,05 segundos).
+
+  function scrollHorizontally() {  //será responsável por mover a rolagem horizontalmente.
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+          container.scrollLeft = 0;  //Se o contêiner estiver no final do conteúdo, a rolagem é reiniciada para o início (posição 0)
+      } else {
+          container.scrollLeft += scrollAmount;
+      }
+  }
+
+  var scrollInterval = setInterval(scrollHorizontally, delay);
+}
