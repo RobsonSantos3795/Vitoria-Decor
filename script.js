b8b3795aa5b6a7ec9b7ab345f83329fcb1a5b700
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-06-24 20:41:27 -0300

    Adição dos kits infantis

---
+++ script.js
@@ -0,0 +1,19 @@
+function entrar1() {
+    location.href = "https://wa.me/p/6523991331010581/557186172127";
+}
+function entrar2() {
+    location.href = "https://wa.me/p/6992854067414800/557186172127";
+}
+function entrar3() {
+    location.href = "https://wa.me/p/7179997458696542/557186172127";
+}
+function entrar4() {
+    location.href = "https://wa.me/p/6504919806269393/557186172127";
+}
+function entrar5() {
+    location.href = "https://wa.me/p/6525171560865534/557186172127";
+}
+function entrar6() {
+    location.href ="https://wa.me/p/7307831705914645/557186172127";
+}
+
