b9e3394d72ed6fda5961c9decf67d7c82fb02edb
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-06-27 23:02:52 -0300

    Atualizando encaminhamento de link do botão

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function entrar1() {
-    location.href = "https://wa.me/p/6523991331010581/557186172127";
+    location.href = "https://wa.me/557186172127";
 }
 function entrar2() {
-    location.href = "https://wa.me/p/6992854067414800/557186172127";
+    location.href = "https://wa.me/557186172127";
 }
 function entrar3() {
-    location.href = "https://wa.me/p/7179997458696542/557186172127";
+    location.href = "https://wa.me/557186172127";
 }
 function entrar4() {
-    location.href = "https://wa.me/p/6504919806269393/557186172127";
+    location.href = "https://wa.me/557186172127";
 }
 function entrar5() {
-    location.href = "https://wa.me/p/6525171560865534/557186172127";
+    location.href = "https://wa.me/557186172127";
 }
 function entrar6() {
-    location.href ="https://wa.me/p/7307831705914645/557186172127";
+    location.href ="https://wa.me/557186172127";
 }
 
+/*p/6523991331010581/
+p/6992854067414800/
+p/7179997458696542/
+p/6504919806269393/
+p/6525171560865534/
+p/7307831705914645/ */
